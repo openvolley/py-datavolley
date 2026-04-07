f2552c7006d526c92afcb2b029d74f5e9fa651c5
tyler-widdison <tylerperrywiddison@gmail.com>
2026-04-06 23:54:33 -0600

    stronger type checking - refactoring

---
+++ datavolley/types/__init__.py
@@ -0,0 +1,15 @@
+from .plays import (
+    ParseIssue,
+    PlayNormalized,
+    PlayRaw,
+    ValidationMode,
+    validate_and_normalize_plays,
+)
+
+__all__ = [
+    "ParseIssue",
+    "PlayRaw",
+    "PlayNormalized",
+    "ValidationMode",
+    "validate_and_normalize_plays",
+]
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 Example usage of the datavolley package to extract and parse volleyball match data.
 """
